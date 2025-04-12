797a31e5bdefee4cefdf70ee52963236edbf2f42
ConcerroX <concerrox@outlook.com>
2025-04-12 17:05:28 +0800

    Add chest bubble

--- effective/common/src/main/kotlin/concerrox/effective/registry/ModParticles.kt
+++ effective/common/src/main/kotlin/concerrox/effective/registry/ModParticles.kt
@@ -1,8 +1,10 @@
 package concerrox.effective.registry
 
 import concerrox.effective.Effective
+import concerrox.effective.particle.BubbleParticle
 import concerrox.effective.particle.CascadeCloudParticle
 import concerrox.effective.particle.DropletParticle
+import concerrox.effective.particle.EndBubbleParticle
 import concerrox.effective.particle.GlowingCascadeCloudParticle
 import concerrox.effective.particle.GlowingDropletParticle
 import concerrox.effective.particle.GlowingRippleParticle
@@ -32,6 +34,8 @@ object ModParticles {
     val GLOWING_DROPLET by register("glowing_droplet", GlowingDropletParticle::Provider) { SimpleParticleType(true) }
     val RIPPLE by register("ripple", RippleParticle::Provider) { SimpleParticleType(true) }
     val GLOWING_RIPPLE by register("glowing_ripple", GlowingRippleParticle::Provider) { SimpleParticleType(true) }
+    val BUBBLE by register("bubble", BubbleParticle::Provider) { SimpleParticleType(true) }
+    val END_BUBBLE by register("end_bubble", EndBubbleParticle::Provider) { SimpleParticleType(true) }
 
     private fun <I : ParticleType<T>, T : ParticleOptions> register(
         id: String, provider: (SpriteSet) -> ParticleProvider<T>, type: () -> I
@@ -43,7 +47,7 @@ object ModParticles {
     }
 
     data class ParticleEntry<I : ParticleType<T>, T : ParticleOptions>(
-        val id: ResourceLocation, val type: Lazy<I>, val provider: (SpriteSet) -> ParticleProvider<T>
+        val id: ResourceLocation, val type: Lazy<I>, val provider: (SpriteSet) -> ParticleProvider<T>, var extra: Any? = null
     )
 
 }
